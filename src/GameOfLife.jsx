c1d8c0be644e5eb0506bc87f17890bfc7bd9642a
Bennett Garcia <bcg77@scarletmail.rutgers.edu>
2024-09-25 12:19:46 -0400

    Test much higher frequency

--- src/GameOfLife.jsx
+++ src/GameOfLife.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import "./GameOfLife.css";
 
 const GameOfLife = ({
-  speed = 1250,
+  speed = 2250,
   maxRotation = 1,  // Maximum rotation angle
   rotationPeriod = 30000,  // Time in ms for a full rotation cycle
   cellSize = 50,
